bb4f8ff617339ed31a6e6907e478304ef82685fd
Byron Jones <byron@glob.com.au>
2018-03-16 11:38:57 +0800

    fix butchered refactor

--- webview.js
+++ webview.js
@@ -13,15 +13,12 @@ module.exports = Franz => {
         // wait for document to load
         if (document.readyState !== 'complete') {
             window.setTimeout(initialise, 50);
-        }
-        // wait for xhr
-        var loading_el = document.querySelector('.dashboard-pane .phui-property-list-text-content');
-        if (!loading_el || loading_el.innerHTML.trim() === 'Loading...') {
-            window.setTimeout(initialise, 50);
+            return;
         }
         // review-requested page only
 		if (document.location.href !== 'https://github.com/pulls/review-requested') {
             window.setTimeout(initialise, 250);
+            return;
         }
 
         // set badge
@@ -39,12 +36,10 @@ module.exports = Franz => {
             // allow clicking on the "review requests" link to refresh the page
             if (el.getAttribute('aria-label') === 'Pull Requests requesting your review') return;
 
-            // allow auth
-            if (el.href.startsWith('/auth')) return;
-
             el.addEventListener('click', open_in_browser);
         });
     };
+    initialise();
 
     // auto-refresh
     window.setInterval(function() {
